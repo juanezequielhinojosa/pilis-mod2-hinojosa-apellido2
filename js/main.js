7ca7d31e79ea08cd46a3999e07fbfc70a8d301bd
FabianDonatelo <tolabaf@gmail.com>
2022-08-25 17:55:43 -0300

    se agrega comentario respecto datos para la api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+//APLICACION DEL CLIMA
+/*function obtenerClima(direccion){
+  console.log('clima');
+ 
+  fetch(direccion, {
+ 
+   })
+  .then((response) => response.json())
+  .then((json) => { 
+      console.log(json);
+     
+  })
+  .catch((err) => console.log('no funciona'+err));
+}*/
+
 //SEGUNDO PASO DE FORMUALRIO
 function onClick(event) {
     event.preventDefault();
@@ -51,9 +66,10 @@ boton.addEventListener("click", onClick);
 
 //AQUI INICIA APLICACION DEL CLIMA
 //DATOS DE LA CIUDAD CULTURAL
-let latitud = -24.183310220963904;
-let longitud = -65.33129600437834;
-let llave = "65a7805889ca0230a262d2c066fc9911";
+// estos son datos geograficos de la ciudad cultural, utilizados para la API
+//let latitud = -24.183310220963904;
+//let longitud = -65.33129600437834;
+//let llave = "65a7805889ca0230a262d2c066fc9911";
 const direccion =
   "https://api.openweathermap.org/data/2.5/weather?lat=-24.183310220963904&lon=-65.33129600437834&appid=65a7805889ca0230a262d2c066fc9911&units=metric&lang=es";
 document.addEventListener("DOMContentLoaded", obtenerClima(direccion));
