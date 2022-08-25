d39cc724cf136b740a35da4a42e5f9d406ce3703
FabianDonatelo <tolabaf@gmail.com>
2022-08-25 17:58:29 -0300

    se modifico comentario respecto los  datos para la  Api del clima

--- js/main.js
+++ js/main.js
@@ -67,9 +67,9 @@ boton.addEventListener("click", onClick);
 //AQUI INICIA APLICACION DEL CLIMA
 //DATOS DE LA CIUDAD CULTURAL
 // estos son datos geograficos de la ciudad cultural, utilizados para la API
-//let latitud = -24.183310220963904;
-//let longitud = -65.33129600437834;
-//let llave = "65a7805889ca0230a262d2c066fc9911";
+//latitud = -24.183310220963904;
+// longitud = -65.33129600437834;
+// llave = "65a7805889ca0230a262d2c066fc9911";
 const direccion =
   "https://api.openweathermap.org/data/2.5/weather?lat=-24.183310220963904&lon=-65.33129600437834&appid=65a7805889ca0230a262d2c066fc9911&units=metric&lang=es";
 document.addEventListener("DOMContentLoaded", obtenerClima(direccion));
